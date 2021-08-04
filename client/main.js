aa765ea373efdab2f7a4dfaef259305ab83254c1
ibn5x <87243859+ibn5x@users.noreply.github.com>
2021-08-04 11:14:02 -0400

    implemented metamask integration via Moralis SDK

--- client/main.js
+++ client/main.js
@@ -1,14 +1,28 @@
 Moralis.initialize("Io42YM9atPkPahd1pfkU4aclVzupfetDimHaXB2OD"); // Application id from moralis.io
 Moralis.serverURL = "https://ktg0yprtbe91.usemoralis.com:2053/server"; //Server url from moralis.io
 
-async function login() {
+async function init() {
     try {
-        user = await Moralis.Web3.authenticate();
-        console.log(user);
-        alert("User logged in")
-    } catch (error) {
-        console.log(error);
-    }
+            let user = Moralis.User.current();
+            if(!user)
+                {
+                    $('login_button').click( () => {
+                        user = await Moralis.Web3.authenticate();
+                    })       
+                }
+            renderWalletMonsters();
+        } catch (error) {
+            console.log(error);
+        }
 }
+
+//Render NFTs here
+function renderWalletMonsters() 
+    {
+
+    }
+
+
+init()
 
 document.getElementById("login_button").onclick = login;
